5e719ac585293041a06a4c9922df14c6be9b9a14
Matti Tuomikoski <mtuomikoski@gmail.com>
2020-08-22 03:27:37 +0300

    Allow googleapis.com access in Travis

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,8 +17,10 @@ require 'webmock/rspec'
 # connection (Selenium)
 if ENV['TRAVIS'].nil? || ENV['TRAVIS'] == 'false'
   WebMock.disable_net_connect!(allow_localhost: true, allow: ENV['SELENIUM_HOST'])
-else
-  WebMock.disable_net_connect!(allow_localhost: true)
+end
+if ENV['TRAVIS'] == 'true'
+  # Allow googleapis.com for Selenium Chromedriver access
+  WebMock.disable_net_connect!(allow_localhost: true, allow: 'googleapis.com')
 end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
