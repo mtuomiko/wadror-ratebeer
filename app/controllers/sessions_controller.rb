18ccbc6f879b8f88947805c212abf468a39ec735
Matti Tuomikoski <mtuomikoski@gmail.com>
2020-09-11 22:23:31 +0300

    Security updates
    -update Rails actionview
    -reset session before login
    -require master key in config
    -load beer img with https

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
     if user&.closed
       redirect_to signin_url, notice: 'Your account is closed, please contact admin'
     elsif user&.authenticate(params[:password])
+      reset_session
       session[:user_id] = user.id
       redirect_to user, notice: 'Welcome back!'
     else
@@ -19,7 +20,7 @@ class SessionsController < ApplicationController
 
   def destroy
     # Clear the session
-    session[:user_id] = nil
+    session.delete :user_id
     # Redirect to main page
     redirect_to :root
   end
