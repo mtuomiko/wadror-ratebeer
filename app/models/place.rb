b2df3e61b62e849541a5bce3dcf5a58e0f496523
Matti Tuomikoski <mtuomikoski@gmail.com>
2022-08-04 01:53:46 +0300

    Update ruby, ignore OpenStruct

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
+# Not going to refactor this away at this point
+# rubocop:disable Style/OpenStructUse
 class Place < OpenStruct
   def self.rendered_fields
     [:id, :name, :status, :street, :city, :zip, :country, :overall]
   end
 end
+# rubocop:enable Style/OpenStructUse
